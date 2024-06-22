ecd918fb47c1d3fe6c8caee097581f7be0e344ff
Sureshcr7 <164998901+Sureshcr7@users.noreply.github.com>
2024-06-22 15:27:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Form submission with alert
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // You can add code to send form data to a server here
+});
